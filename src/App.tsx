b07884c14b973424c233ae229de5385f4ad3d670
Jiyaho <masqu2n@gmail.com>
2023-02-01 01:07:50 +0900

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import About from './routes/About';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
